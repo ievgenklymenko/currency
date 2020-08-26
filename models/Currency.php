2af8730b7a5e45643456ff01561c9be050c9e99e
Ievgen Klymenko <ievgenklymenko@gmail.com>
2020-08-26 16:57:38 +0300

    create CurrencyDataGateway and currency sync method

--- models/Currency.php
+++ models/Currency.php
@@ -2,8 +2,17 @@
 
 namespace app\models;
 
+use app\gateways\CurrencyDataGateway;
 use yii\db\ActiveRecord;
 
 class Currency extends ActiveRecord
 {
+    public static function sync()
+    {
+        $insertRows = CurrencyDataGateway::get();
+
+        $db = \Yii::$app->db;
+        $sql = $db->queryBuilder->batchInsert(static::tableName(), ['code', 'name', 'rate'], $insertRows);
+        $db->createCommand("$sql AS new ON DUPLICATE KEY UPDATE rate=new.rate")->execute();
+    }
 }
